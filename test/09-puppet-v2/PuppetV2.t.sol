0a50053b6bece8aebcba394587795f3110b70809
bentobox19.eth <bentobox19@protonmail.com>
2023-04-29 21:05:23 -0400

    Getting out from the chaos 0

--- test/09-puppet-v2/PuppetV2.t.sol
+++ test/09-puppet-v2/PuppetV2.t.sol
@@ -2,13 +2,13 @@
 pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
-import "../../src/puppet-v2/PuppetV2Level.sol";
+// import "../../src/puppet-v2/PuppetV2Level.sol";
 
 contract PuppetV2Test is Test {
-  PuppetV2Level level = new PuppetV2Level();
+  // PuppetV2Level level = new PuppetV2Level();
 
   function setUp() public {
-    level.setup();
+    // level.setup();
   }
 
   function testExploit() public {
